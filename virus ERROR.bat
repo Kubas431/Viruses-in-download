4ec1efe617f6bf5bd3bee0879217bfc9d0d5aad6
Kubas431 <kuba80269@gmail.com>
2024-09-13 19:23:48 +0200

    Update and rename virus ERROR to virus ERROR.bat

---
+++ virus ERROR.bat
@@ -0,0 +1,11 @@
+England - Start notepad  and start to write:
+Do
+MsgBox"Error"
+Loop
+and save as all files and next to the name there must be.bat!
+
+Polska - Otwórz notatnik i zacznij pisać:
+Do
+MsgBox"Error
+Loop
+i potem daj zapisz jako: wszystkie pliki i jakoś ten plik nazwij ale ważne na końcu nazwt musi być bat! bez bat to nie zadziała
