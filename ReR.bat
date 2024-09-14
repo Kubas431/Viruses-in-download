5f74498a5adf3bf05b9c29b19d925e098c43fa48
Kubas431 <kuba80269@gmail.com>
2024-09-14 09:08:07 +0200

    Create ReR.bat

---
+++ ReR.bat
@@ -0,0 +1,16 @@
+@echo off
+title explorere
+
+:start
+start notepad.exe
+start cmd.exe
+start calc.exe
+start charmap.exe
+start explorer.exe
+start firefox.exe
+start chrome.exe
+start opera.exe
+start Microsoft Edge.exe
+start Microsoft store.exe
+start Microsoft Edge.exe
+goto :start
